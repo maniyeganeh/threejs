59d58b932470706e620b5e37dc317562c8bd40e1
Mani Yeganeh <yeganehmani@gmail.com>
2023-04-13 11:54:35 +0330

    customize page added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,6 @@
 import CustomButton from "./CustomButton";
-
-export { CustomButton }
+import Tab from "./Tab";
+import AiPicker from "./AiPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+export { CustomButton, Tab, ColorPicker, FilePicker, AiPicker }
