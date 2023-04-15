827811b2c4f19c1147dd5e7bd1d8f0601b86d508
Mani Yeganeh <yeganehmani@gmail.com>
2023-04-15 10:35:58 +0330

    ai added

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -45,10 +45,26 @@ const Customizer = () => {
     }
   };
   const handleSubmit = async (type) => {
-    if (!prompt) return alert('Please Enter a Promp');
+    if (!prompt) return alert('Please enter a prompt');
+
     try {
-    } catch (err) {
-      alert(err);
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
+    } catch (error) {
+      alert(error);
     } finally {
       setGeneratingImg(false);
       setActiveEditorTab('');
--- server/routes/dalle-routes.js
+++ server/routes/dalle-routes.js
@@ -4,12 +4,33 @@ import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
 
-const router = express.Router()
+const router = express.Router();
+
+const config = new Configuration({
+    apiKey: process.env.OPEN_AI_API_KEY
+})
+const openai = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
     res.status(200).json({ message: "Hello From DALL-E Routes" })
 
 
+})
+router.route("/").post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64_json"
+        })
+        const image = response.data.date[0].b64_json
+        res.status(200).json({ photo: image })
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 export default router
